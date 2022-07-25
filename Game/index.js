352c32590de681874593f50f620eddec897ab1d3
Lucas <rukasubahamut@gmail.com>
2022-07-24 23:54:22 -0300

    Adicionado player e movimentação

--- Game/index.js
+++ Game/index.js
@@ -4,19 +4,220 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+const collisionsMap = []
+//Lagura do mapa 70
+for (let i = 0; i < collisions.length; i += 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
 
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        //blocos de 12px, mas como mapa foi exportado a 400% -> 12x4
+        this.width = 48
+        this.height = 48
+    }
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+
+const offset = {
+    x: -1025,
+    y: -760
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025) {
+            boundaries.push(new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            }))
+        }
+
+    })
+})
+
 
 
 const image = new Image();
 image.src = './img/Pellet Town.png'
-console.log(image)
 
-image.onload = () => {
-    c.drawImage(image, -1100, -760) //param 1 = imagem, eixoX, eixoY
+const playerImage = new Image();
+playerImage.src = './img/playerDown.png'
+
+class Sprite {
+    constructor({ position, velocity, image, frames = { max: 1 } }) {
+        this.position = position
+        this.image = image
+        this.frames = frames
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max
+            this.height = this.image.height
+        }
+    }
+
+    draw() {
+        c.drawImage(this.image,
+            0, //coordX para recorta
+            0, //coordy para recorta
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height
+        )
+    }
 }
 
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - (192 / 4) / 2, //centrelizando player
+        y: canvas.height / 2 - 68 / 2
+    },
+    image: playerImage,
+    //recortando a imagem em 4
+    frames: {
+        max: 4
+    }
+})
+
+const background = new Sprite({
+    position: {
+        x: offset.x,
+        y: offset.y
+    },
+    image: image,
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+
+
+const movables = [background, ...boundaries]
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
+
+//Renderização
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+
+    //colisão 
+    boundaries.forEach(boundary => {
+        boundary.draw()
+        if (rectangularCollision({
+            rectangle1: player,
+            rectangle2: boundary
+        })) {
+            console.log("colisão")
+        }
+    })
+    player.draw()
+
+
+
+    //Movimentação do personagem
+    if (keys.w.pressed && lastKey === 'w') {
+        movables.forEach(movable => {
+            movable.position.y += 3
+        })
+    } else if (keys.a.pressed && lastKey === 'a') {
+        movables.forEach(movable => {
+            movable.position.x += 3
+        })
+    } else if (keys.s.pressed && lastKey === 's') {
+        movables.forEach(movable => {
+            movable.position.y -= 3
+        })
+    } else if (keys.d.pressed && lastKey === 'd') {
+        movables.forEach(movable => {
+            movable.position.x -= 3
+        })
+    }
+
+}
+animate()
+
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+            break;
+
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break;
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break;
+        case 'd':
+            lastKey = 'd'
+            keys.d.pressed = true
+
+            break;
+    }
+
+
+})
+
+window.addEventListener('keyup', (e) => {
+
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break;
+
+        case 'a':
+            keys.a.pressed = false
+
+            break;
+        case 's':
+            keys.s.pressed = false
+
+            break;
+        case 'd':
+            keys.d.pressed = false
+
+            break;
+    }
+
+
+})
 
 
